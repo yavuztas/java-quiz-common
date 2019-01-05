935c0e5e7d081dad4eec50a7989bf820c89fd888
Yavuz Tas <yavuzslmtas@gmail.com>
2019-01-05 19:29:06 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Solutions for common java algorithm questions
+I have rearranged my java codes mainly used for online quizes before, to form a collection of solutions for common java algorithm and data structure questions. Note that Java 8 is required to build this project. Hope it helps to fellow developers!
+
+### Path Question:
+Write a function that provides change directory (cd) function for an abstract file system.<br>
+<br>Specification:<br>
+* Root path is '/'.
+* Path separator is '/'.
+* Parent directory is addressable as "..".
+* Directory names consist only of English alphabet letters (A-Z and a-z).
+* The function should support both relative and absolute paths.
+* The function will not be passed any invalid paths.
+* Do not use built-in path-related functions.
+
+For example:<br>
+```java
+Path path = new Path("/a/b/c/d");
+path.cd('../x');
+System.out.println(path.getPath());
+```
+<br>should display '/a/b/c/x'.
+
+
---
+++ src/org/java/quiz/Path.java
@@ -0,0 +1,99 @@
+package org.java.quiz;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Solution for a function that provides change directory (cd) function for an
+ * abstract file system.
+ * 
+ * @author Yavuz Tas
+ */
+public class Path {
+
+	private String path;
+	private List<String> folders;
+	private int currentFolderLevel = 0;
+
+	public Path(String path) {
+		this.path = path;
+		this.createPath(path);
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void cd(String newPath) {
+
+		if (newPath.equals("/")) {
+			this.path = "/";
+			return;
+		}
+
+		if (newPath.equals(".") || newPath.equals("./")) {
+			return;
+		}
+
+		if (newPath.equals("..") || newPath.equals("../")) {
+			this.folders.remove(currentFolderLevel);
+			this.currentFolderLevel = this.folders.size() - 1;
+			this.path = String.join("/", folders);
+			return;
+		}
+
+		String cleared = newPath.replaceAll("[\\./]", "");
+		if (!cleared.isEmpty() && !cleared.matches("[A-Za-z]+")) {
+			return;
+		}
+
+		if (newPath.startsWith("/")) {
+			createPath(newPath);
+			this.path = String.join("/", folders);
+			return;
+		}
+
+		if (newPath.startsWith("../")) {
+			this.folders.remove(currentFolderLevel);
+			appendPath(newPath.substring(3));
+			this.path = String.join("/", folders);
+			return;
+		}
+
+		if (newPath.startsWith("./")) {
+			appendPath(newPath.substring(2));
+			this.path = String.join("/", folders);
+		} else {
+			appendPath(newPath);
+			this.path = String.join("/", folders);
+		}
+
+	}
+
+	public void createPath(String clearPath) {
+		this.folders = new ArrayList<>(Arrays.asList(clearPath.split("/")));
+		this.currentFolderLevel = this.folders.size() - 1;
+	}
+
+	public void appendPath(String clearPath) {
+		List<String> names = new ArrayList<>(Arrays.asList(clearPath.split("/")));
+		for (String name : names) {
+			if (name.equals(".")) {
+				continue;
+			}
+			if (name.equals("..")) {
+				this.folders.remove(this.folders.size() - 1);
+			} else {
+				this.folders.add(name);
+			}
+		}
+		this.currentFolderLevel = this.folders.size() - 1;
+	}
+
+	public static void main(String[] args) {
+		Path path = new Path("/a/b/c/d");
+		path.cd("../x");
+		System.out.println(path.getPath());
+	}
+}
